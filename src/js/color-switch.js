6a584f0870c10cb13a86970db7a8144d31aa7a4f
Nadya Mincheva <7000585m1@gmail.com>
2021-10-24 12:56:51 +0300

    add

--- src/js/color-switch.js
+++ src/js/color-switch.js
@@ -18,38 +18,34 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const switchColors = {
+const colorsSwitch = {
   intervalId: null,
 
   start() {
-    // console.dir('start', refs.btnStart);
 
     refs.btnStart.disabled = true;
     refs.btnStop.disabled = false;
 
     this.intervalId = setInterval(() => {
       const currentIndex = randomIntegerFromInterval(0, colors.length - 1);
-      // console.log(currentIndex);
 
       colors.map((el, ind) => {
         if (currentIndex === ind) {
           document.body.style.backgroundColor = el;
-          // console.log(el);
         }
       });
     }, 1000);
   },
 
   stop() {
-    // console.dir('stop', refs.btnStop);
+
 
     refs.btnStop.disabled = true;
     refs.btnStart.disabled = false;
-
     clearInterval(this.intervalId);
     this.intervalId = null;
   },
 };
 
-refs.btnStart.addEventListener('click', switchColors.start.bind(switchColors));
-refs.btnStop.addEventListener('click', switchColors.stop.bind(switchColors));
+refs.btnStart.addEventListener('click', colorsSwitch.start.bind(colorsSwitch));
+refs.btnStop.addEventListener('click', colorsSwitch.stop.bind(colorsSwitch));
